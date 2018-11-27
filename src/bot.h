3eb0f3e2bbdb8974aba0f289466e29b15dffb74f
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2018-11-28 00:18:47 +0100

    port defines added

--- src/bot.h
+++ src/bot.h
@@ -12,6 +12,46 @@
 #include <inttypes.h>
 #include "Arduino.h"
 
+///////////////////////
+// some port defines //
+///////////////////////
+
+// analog ports 
+#define BOT_A1
+#define BOT_A2
+#define BOT_A3
+#define BOT_A4
+#define BOT_A5
+#define BOT_A6
+
+// digital ports
+#define BOT_D1
+#define BOT_D2
+#define BOT_D3
+#define BOT_D4
+#define BOT_D5
+#define BOT_D6
+
+// button ports
+#define BOT_B1
+#define BOT_B2
+#define BOT_B3
+#define BOT_B4
+
+// servo ports
+#define BOT_S1
+#define BOT_S2
+#define BOT_S3
+#define BOT_S4
+
+// led ports 
+#define BOT_LED1
+#define BOT_BOT_LED2
+#define BOT_BOT_LED3
+#define BOT_BOT_LED4
+#define BOT_BOT_LED5
+#define BOT_BOT_LED6
+
 class bot
 {
 	private:
