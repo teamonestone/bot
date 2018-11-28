98a136255da832e1c1b77866c1f89aa12cb0584c
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2018-11-28 13:16:32 +0100

    function pointer update

--- src/bot.cpp
+++ src/bot.cpp
@@ -339,18 +339,18 @@ void bot::micro_delay(uint32_t _delay_time) {
     while (micros() <= _time_to_wait);
 }
 
-void bot::delay_f(uint32_t _delay_time, void (*_funct)()) {
+void bot::delay_f(uint32_t _delay_time, void (*_funct)(uint64_t)) {
     uint64_t _time_to_wait = millis() + _delay_time;
 
     while (millis() <= _time_to_wait) {
-        _funct();
+        _funct(_time_to_wait);
     }
 }
 
-void bot::micro_delay_f(uint32_t _delay_time, void (*_funct)())) {
+void bot::micro_delay_f(uint32_t _delay_time, void (*_funct)(uint64_t))) {
     uint64_t _time_to_wait = micros() + _delay_time;
 
     while (micros() <= _time_to_wait) {
-        _funct();
+        _funct(_time_to_wait);
     }
 }
