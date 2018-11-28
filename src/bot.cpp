a0ac827aa7dc895d2d353fac19b12f6c56077eae
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2018-11-28 11:01:32 +0100

    led & button functions implemented

--- src/bot.cpp
+++ src/bot.cpp
@@ -55,29 +55,135 @@ bot::~bot() {
 bool button(uint8_t button) {
     // check if input is in range
     if (button < 1 || button > 4) return false;
+
+    switch(button) {
+        case 1: 
+            return ~(digitalRead(BOT_B1));
+
+        case 2:
+            return ~(digitalRead(BOT_B2));
+
+        case 3:
+            return ~(digitalRead(BOT_B3));
+
+        case 4:
+            return ~(digitalRead(BOT_B4));
+
+        default:
+            return false;
+    }
 }
 
 void wait_botton(uint8_t button) {
     // check if input is in range
     if (button < 1 || button > 4) return;
+
+    switch(button) {
+        case 1: 
+            while(digitalRead(BOT_B1));
+        break;
+
+        case 2:
+            while(digitalRead(BOT_B2));
+        break;
+
+        case 3:
+            while(digitalRead(BOT_B3));
+        break;
+
+        case 4:
+            while(digitalRead(BOT_B4));
+        break;
+
+        default:
+            return;
+    }
 }
 
 void leds_on() {
-    
+    digitalWrite(BOT_LED1, 1);
+    digitalWrite(BOT_LED2, 1);
+    digitalWrite(BOT_LED3, 1);
+    digitalWrite(BOT_LED4, 1);
+    digitalWrite(BOT_LED5, 1);
+    digitalWrite(BOT_LED6, 1);
 }
 
 void leds_off() {
-
+    digitalWrite(BOT_LED1, 0);
+    digitalWrite(BOT_LED2, 0);
+    digitalWrite(BOT_LED3, 0);
+    digitalWrite(BOT_LED4, 0);
+    digitalWrite(BOT_LED5, 0);
+    digitalWrite(BOT_LED6, 0);
 }
 
 void led_on(uint8_t led) {
     // check if input is in range
     if (led < 1 || led > 6) return;
+
+    switch(led) {
+        case 1: 
+            digitalWrite(BOT_LED1, 1);
+        break;
+
+        case 2:
+            digitalWrite(BOT_LED2, 1);
+        break;
+
+        case 3:
+            digitalWrite(BOT_LED3, 1);
+        break;
+
+        case 4:
+            digitalWrite(BOT_LED4, 1);
+        break;
+
+        case 5:
+            digitalWrite(BOT_LED5, 1);
+        break;
+
+        case 6:
+            digitalWrite(BOT_LED6, 1);
+        break;
+
+        default:
+            return;
+    }
 }
 
 void led_off(uint8_t led) {
     // check if input is in range
     if (led < 1 || led > 6) return;
+
+    switch(led) {
+        case 1: 
+            digitalWrite(BOT_LED1, 1);
+        break;
+
+        case 2:
+            digitalWrite(BOT_LED2, 1);
+        break;
+
+        case 3:
+            digitalWrite(BOT_LED3, 1);
+        break;
+
+        case 4:
+            digitalWrite(BOT_LED4, 1);
+        break;
+
+        case 5:
+            digitalWrite(BOT_LED5, 1);
+        break;
+
+        case 6:
+            digitalWrite(BOT_LED6, 1);
+        break;
+
+        default:
+            return;
+    }
 }
 
 //////////////////////////
