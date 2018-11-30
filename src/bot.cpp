61607715fcec6a8eb380d73ea4c694a5fde68401
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2018-11-30 15:05:11 +0100

    delays with additional break condition

--- src/bot.cpp
+++ src/bot.cpp
@@ -354,3 +354,15 @@ void bot::micro_delay_f(uint32_t _delay_time, void (*_funct)(uint64_t))) {
         _funct(_time_to_wait);
     }
 }
+
+void delay_with_condition(uint32_t _delay_time, bool (*_condition)(uint64_t)) {
+    uint64_t _time_to_wait = millis() + _delay_time;
+
+    while (millis() <= _time_to_wait && _condition(_time_to_wait);
+}
+
+void micro_delay_with_condition(uint32_t _delay_time, bool (*_condition)(uint64_t)) {
+    uint64_t _time_to_wait = micros() + _delay_time;
+
+    while (micros() <= _time_to_wait && _condition(_time_to_wait);
+}
